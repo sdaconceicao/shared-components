c300f9b02316bc706982e556b3fbd2bef0cea26e
Stephen da Conceicao <daconcei472@gmail.com>
2019-01-19 03:00:05 -0500

    Added helper method for adding unique keys to arrays

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ export * as Forms from './components/Form';
 export Form from './components/Form';
 export * as Modal from './components/Modal';
 export * as enMessages from './translations/en.json';
+
+export * as arrayUtils from './utils/array.util';
---
+++ src/utils/array.util.js
@@ -0,0 +1,11 @@
+/**
+ * Takes an object array and adds keys, useful for react list mapping where array can be modified and index
+ * can change
+ * @param array
+ * @returns array
+ */
+export function addIdsToArrayElements(array){
+    return array.map(value => {
+        return { value, key: Math.floor(Math.random() * 1000+1) };
+    });
+}
