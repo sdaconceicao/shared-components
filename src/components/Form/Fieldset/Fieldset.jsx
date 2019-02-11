7a91cfe652f7d6422517d0d7677df56ad18d5b7d
Stephen da Conceicao <daconcei472@gmail.com>
2019-02-11 02:03:11 -0500

    Exporting fieldset + removing class

--- src/components/Form/Fieldset/Fieldset.jsx
+++ src/components/Form/Fieldset/Fieldset.jsx
@@ -1,18 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class Fieldset extends Component {
-
-    render (){
-        const {children, legend, required} = this.props;
-        return (
-            <fieldset>
-                {legend && <legend>{legend}{required && <span>*</span>}</legend>}
-                {children}
-            </fieldset>
-        )
-    }
-}
+const Fieldset = ({children, legend, required}) => {
+    return (
+        <fieldset>
+            {legend && <legend>{legend}{required && <span>*</span>}</legend>}
+            {children}
+        </fieldset>
+    )
+};
 
 Fieldset.propTypes = {
     children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired
