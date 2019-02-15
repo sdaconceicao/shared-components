7711abce793894b124554446b43e6af517da932f
Stephen da Conceicao <daconcei472@gmail.com>
2019-02-15 01:32:15 -0500

    Updated form styling

--- src/components/Form/FormElement/FormElementWrapper.jsx
+++ src/components/Form/FormElement/FormElementWrapper.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import PropTypes from 'prop-types'
+
 import Label from "../Label";
+
+import './FormElementWrapper.scss';
 
 export const FormElementWrapper = ({children, label, id, required}) => (
     <div className={'form-element'}>
@@ -9,3 +13,14 @@ export const FormElementWrapper = ({children, label, id, required}) => (
 );
 
 export default FormElementWrapper;
+
+FormElementWrapper.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
+    label: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    id: PropTypes.string,
+    required: PropTypes.bool
+};
+
+FormElementWrapper.defaultValues = {
+    required: false
+};
