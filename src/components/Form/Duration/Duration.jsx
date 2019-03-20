a982fb23cf9dc7d209e34c0bb78a502080aba2f7
Stephen da Conceicao <daconcei472@gmail.com>
2019-03-20 02:20:31 -0400

    Shorter duration display values

--- src/components/Form/Duration/Duration.jsx
+++ src/components/Form/Duration/Duration.jsx
@@ -18,8 +18,8 @@ export class Duration extends FormElement {
         value: this.props.value
     };
 
-    hours = Array.from(Array(60), (_, x)=> {return {label: `${x+1} ${x < 1 ? 'hour' : 'hours'}`, value: x+1}});
-    minutes = Array.from(Array(60), (_, x)=> {return {label: `${x+1} ${x < 1 ? 'minute' : 'minutes'}`, value: x+1}});
+    hours = Array.from(Array(60), (_, x)=> {return {label: `${x+1} ${x < 1 ? 'hr' : 'hrs'}`, value: x+1}});
+    minutes = Array.from(Array(60), (_, x)=> {return {label: `${x+1} ${x < 1 ? 'min' : 'mins'}`, value: x+1}});
 
     onChange = (e, type) =>{
         const {onChange} = this.props;
@@ -49,7 +49,7 @@ export class Duration extends FormElement {
                         value={hours}
                         options={this.hours}
                         tabIndex={tabIndex}
-                        placeholder="-----------"
+                        placeholder="-----"
                         onChange={(e)=>this.onChange(e, 'hours')}/>
                 <Select id={`duration-minutes-${id}`}
                         name={`duration-minutes-${name}`}
@@ -57,7 +57,7 @@ export class Duration extends FormElement {
                         value={minutes}
                         options={this.minutes}
                         tabIndex={tabIndex + .1}
-                        placeholder="-----------"
+                        placeholder="-----"
                         onChange={(e)=>this.onChange(e, 'minutes')}/>
             </div>
         )
